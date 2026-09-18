9e856d4ee79ab5dcb16eb54d6085c27e55dd1815
rafi-sunarya <muhammadrafi.sunarya825@gmail.com>
2026-09-18 11:17:07 +0700

    Fix Cloudinary upload method for Vercel

--- app/Http/Controllers/ProductImageController.php
+++ app/Http/Controllers/ProductImageController.php
@@ -49,9 +49,7 @@ class ProductImageController extends Controller
 
         // Handle image upload
         if($request->hasFile('image_url')) {
-            $uploadedFile = Cloudinary::upload($request->file('image_url')->getRealPath(), [
-                'folder' => 'uploads/product-image',
-            ]);
+            $uploadedFile = $request->file('image_url')->storeOnCloudinary('uploads/product-image');
 
             // Ambil URL aman dan public_id untuk file yang diunggah
             $validated['image_url'] = $uploadedFile->getSecurePath(); // URL aman
